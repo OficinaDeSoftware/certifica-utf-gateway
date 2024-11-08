f7efa6ffc49cdd4ae1129b0ddc84e98350e422fe
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2024-11-07 22:32:11 -0300

    feat: #CERUTF-119 Create GatewayService

---
+++ src/main/java/com/certificautfgateway/CertificaUtfGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.certificautfgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificaUtfGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertificaUtfGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/certificautfgateway/CertificaUtfGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certificautfgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificaUtfGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
